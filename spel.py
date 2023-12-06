c52b70b78e29b6740d28589a5d5b7a3ad93d3649
Lukas <lukas.peters@skola.taby.se>
2023-12-06 16:21:35 +0100

    Co-authored-by: haftenmannen11222 <haftenmannen11222@users.noreply.github.com>

--- spel.py
+++ spel.py
@@ -1,16 +1,100 @@
 import random 
 
-print("""                                                                                                                                                                      
-   █     █▓█████ ██▓    ▄████▄  ▒█████  ███▄ ▄███▓█████    ▄▄▄█████▓▒█████      ▄████▄  ▄▄▄     ▓█████ ██▓    ██▓█████▄ 
-  ▓█░ █ ░█▓█   ▀▓██▒   ▒██▀ ▀█ ▒██▒  ██▓██▒▀█▀ ██▓█   ▀    ▓  ██▒ ▓▒██▒  ██▒   ▒██▀ ▀█ ▒████▄   ▓█   ▀▓██▒   ▓██▒██▀ ██▌
-  ▒█░ █ ░█▒███  ▒██░   ▒▓█    ▄▒██░  ██▓██    ▓██▒███      ▒ ▓██░ ▒▒██░  ██▒   ▒▓█    ▄▒██  ▀█▄ ▒███  ▒██░   ▒██░██   █▌
-  ░█░ █ ░█▒▓█  ▄▒██░   ▒▓▓▄ ▄██▒██   ██▒██    ▒██▒▓█  ▄    ░ ▓██▓ ░▒██   ██░   ▒▓▓▄ ▄██░██▄▄▄▄██▒▓█  ▄▒██░   ░██░▓█▄   ▌
-  ░░██▒██▓░▒████░██████▒ ▓███▀ ░ ████▓▒▒██▒   ░██░▒████▒     ▒██▒ ░░ ████▓▒░   ▒ ▓███▀ ░▓█   ▓██░▒████░██████░██░▒████▓ 
-  ░ ▓░▒ ▒ ░░ ▒░ ░ ▒░▓  ░ ░▒ ▒  ░ ▒░▒░▒░░ ▒░   ░  ░░ ▒░ ░     ▒ ░░  ░ ▒░▒░▒░    ░ ░▒ ▒  ░▒▒   ▓▒█░░ ▒░ ░ ▒░▓  ░▓  ▒▒▓  ▒ 
-    ▒ ░ ░  ░ ░  ░ ░ ▒  ░ ░  ▒    ░ ▒ ▒░░  ░      ░░ ░  ░       ░     ░ ▒ ▒░      ░  ▒    ▒   ▒▒ ░░ ░  ░ ░ ▒  ░▒ ░░ ▒  ▒ 
-    ░   ░    ░    ░ ░  ░       ░ ░ ░ ▒ ░      ░     ░        ░     ░ ░ ░ ▒     ░         ░   ▒     ░    ░ ░   ▒ ░░ ░  ░ 
-      ░      ░  ░   ░  ░ ░         ░ ░        ░     ░  ░               ░ ░     ░ ░           ░  ░  ░  ░   ░  ░░    ░    
-                       ░ """)
+import time
+
+def delayed_print_green(text, delay=0.1):
+    for char in text:
+        print('\033[92m' + char + '\033[0m', end='', flush=True)
+        time.sleep(delay)
+
+# Set your desired delay (in seconds) between each character
+delay = 0.0002
+
+# The text to be printed with delays
+text_to_print = """ 
+
+         _____                    _____                    _____                    _____            _____                    _____          
+         /\    \                  /\    \                  /\    \                  /\    \          /\    \                  /\    \         
+        /::\    \                /::\    \                /::\    \                /::\____\        /::\    \                /::\    \        
+       /::::\    \              /::::\    \              /::::\    \              /:::/    /        \:::\    \              /::::\    \       
+      /::::::\    \            /::::::\    \            /::::::\    \            /:::/    /          \:::\    \            /::::::\    \      
+     /:::/\:::\    \          /:::/\:::\    \          /:::/\:::\    \          /:::/    /            \:::\    \          /:::/\:::\    \     
+    /:::/  \:::\    \        /:::/__\:::\    \        /:::/__\:::\    \        /:::/    /              \:::\    \        /:::/  \:::\    \    
+   /:::/    \:::\    \      /::::\   \:::\    \      /::::\   \:::\    \      /:::/    /               /::::\    \      /:::/    \:::\    \   
+  /:::/    / \:::\    \    /::::::\   \:::\    \    /::::::\   \:::\    \    /:::/    /       ____    /::::::\    \    /:::/    / \:::\    \  
+ /:::/    /   \:::\    \  /:::/\:::\   \:::\    \  /:::/\:::\   \:::\    \  /:::/    /       /\   \  /:::/\:::\    \  /:::/    /   \:::\ ___\ 
+/:::/____/     \:::\____\/:::/  \:::\   \:::\____\/:::/__\:::\   \:::\____\/:::/____/       /::\   \/:::/  \:::\____\/:::/____/     \:::|    |
+\:::\    \      \::/    /\::/    \:::\  /:::/    /\:::\   \:::\   \::/    /\:::\    \       \:::\  /:::/    \::/    /\:::\    \     /:::|____|
+ \:::\    \      \/____/  \/____/ \:::\/:::/    /  \:::\   \:::\   \/____/  \:::\    \       \:::\/:::/    / \/____/  \:::\    \   /:::/    / 
+  \:::\    \                       \::::::/    /    \:::\   \:::\    \       \:::\    \       \::::::/    /            \:::\    \ /:::/    /  
+   \:::\    \                       \::::/    /      \:::\   \:::\____\       \:::\    \       \::::/____/              \:::\    /:::/    /   
+    \:::\    \                      /:::/    /        \:::\   \::/    /        \:::\    \       \:::\    \               \:::\  /:::/    /    
+     \:::\    \                    /:::/    /          \:::\   \/____/          \:::\    \       \:::\    \               \:::\/:::/    /     
+      \:::\    \                  /:::/    /            \:::\    \               \:::\    \       \:::\    \               \::::::/    /      
+       \:::\____\                /:::/    /              \:::\____\               \:::\____\       \:::\____\               \::::/    /       
+        \::/    /                \::/    /                \::/    /                \::/    /        \::/    /                \::/____/        
+         \/____/                  \/____/                  \/____/                  \/____/          \/____/                  ~~       
+                                                                        
+
+"""
+
+# Call the delayed_print_green function with the specified text and delay
+delayed_print_green(text_to_print, delay)
+
+#Åvanför är koden för själva "Welcome to Caelid" intron
+
+def delayed_print2_green(text, delay=0.1):
+    for char in text:
+        print('\033[92m' + char + '\033[0m', end='', flush=True)
+        time.sleep(delay)
+
+# Set your desired delay (in seconds) between each character
+delay = 0.001
+
+# The text to be printed with delays
+text_to_print2 = """
+                    
+⢰⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼
+⠀⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠴⠶⠖⠒⠛⣿⣽⡟⠛⠓⠒⠶⠶⢤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠴⣯⠏
+⠀⠀⢿⣯⣿⠷⣶⣤⡴⣶⣶⣤⠤⠤⣤⣄⡀⠀⣀⡤⠶⠛⠉⠀⠀⠀⠀⠀⠀⠴⠿⣼⠿⠦⠀⠀⠀⠀⠀⠀⠉⠛⠲⢦⣄⣀⣤⣴⠒⣻⣿⣿⢻⣿⣿⣿⠋⣩⣴⠋⠀
+⠀⠀⠀⠙⠿⣦⣼⣟⢷⣷⢹⣿⣌⣿⡟⢺⣿⠛⠻⣄⠀⠀⠀⠀⢀⣠⣤⠤⠖⠒⠒⠛⠒⠒⠒⠦⠤⣤⣀⠀⠀⢀⣤⠖⠛⢿⣇⠐⡾⣷⣿⡟⢚⣿⣷⣿⠶⠋⠁⠀⠀
+⠀⠀⠀⠀⠀⠈⠙⠛⠛⠻⠾⢿⣾⣮⣗⣸⣿⣆⠄⠀⠙⣦⡖⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢉⣷⡟⠀⡀⢨⣽⣿⣽⣶⢿⡿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠛⠉⠙⠻⢿⣷⣶⡂⣸⡟⠓⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠛⣧⣄⣿⣾⣿⡋⠉⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠟⠁⠀⠀⠀⣠⠾⣿⣿⣿⣿⡁⠀⠈⢳⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠋⠀⠀⣿⣿⣿⣫⣶⠟⢦⡀⠀⠀⣀⠹⣆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡼⢿⡷⣾⠀⢀⡞⠁⠀⠹⡄⢻⣿⣿⡆⠀⠘⣿⣦⣤⣀⣀⠀⠀⣀⣀⣤⣶⣿⡯⠀⢀⣾⣿⡿⠋⢀⡞⠀⠀⠙⢆⣀⣿⣻⣯⣷⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡾⠷⠛⢳⡞⣻⠋⠀⠀⠀⠀⢳⡀⢻⣿⣿⣦⣠⣿⡯⣷⡉⣽⠿⠿⠟⠉⣹⡯⣿⣷⣤⣾⣿⣿⠁⠀⣼⠃⠀⠀⠀⠈⠻⡍⠏⠁⠉⢷⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠈⡱⠁⠀⠀⠀⠀⠀⠈⢷⠈⣿⣿⣿⠟⣿⣇⡝⣮⡈⠀⠀⠀⣴⡟⠀⢿⡟⢿⣻⣿⣇⠀⣰⠇⠀⠀⠀⠀⠀⠀⠙⡄⠀⠀⠀⢿⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⡼⠃⠀⠀⠀⣀⣀⣠⣤⣼⠿⣿⢿⠃⣰⠋⠈⠁⢻⡙⢶⣴⠟⣹⠃⠀⠀⠱⡄⢹⣿⣟⠲⢿⠤⠤⣤⣤⣀⡀⠀⠀⠸⡆⠀⠀⠈⣧⠀⠀⠀
+⠀⠀⠀⠀⢠⡏⠀⠀⠀⣰⣃⣤⠖⠋⣉⡡⢆⣠⠟⠁⣼⣿⡿⢸⣇⠶⠊⠀⢸⣷⠛⠉⢳⣿⠀⠀⠐⢶⠹⡌⣿⡿⣆⠈⠱⢦⡐⠦⣄⣉⣙⣶⣄⣹⡀⠀⠀⢸⡇⠀⠀
+⠀⠀⠀⠀⣾⠀⠀⢰⣶⣿⣿⣿⡿⢿⣥⣶⣟⣁⣠⣞⣽⠟⡇⢸⣿⡀⣀⡴⠋⢹⡄⠀⣸⣉⣻⣦⣄⣸⣰⡇⣿⢹⣮⣷⣤⣤⣿⠿⠞⣛⣿⣿⣿⣿⡿⠂⠀⠀⢿⠀⠀
+⠀⠀⠀⠀⡟⠀⠀⠀⢹⠋⠳⢿⣿⣷⡶⠦⢭⣽⣾⣿⡟⠰⢷⣘⣿⠁⠿⠋⠉⠙⣿⠉⡿⠉⠉⠉⠏⢩⣿⢠⣟⣐⣿⣿⢷⣾⣷⣒⣩⣿⠿⠟⠉⠀⢱⠀⠀⠀⢸⠀⠀
+⠀⠀⠀⢠⡇⠀⠀⠀⢸⠀⠀⠀⠈⠙⠻⠿⢿⣿⣿⣿⡟⣠⣾⣳⣽⣷⣦⢠⠄⣖⢹⣿⠃⠃⠠⠂⣰⣿⣿⢿⣧⣄⣻⣿⣿⣛⠟⠛⠋⠀⠀⠀⠀⠀⢸⡄⠀⠀⢸⡇⠀
+⠀⠀⠀⢸⡇⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⢀⣸⠿⠋⢻⣿⣿⣿⣿⡽⣽⣾⣿⣦⣬⠞⠏⠀⢤⣼⣿⣿⣿⢱⣿⣿⣿⣿⡆⠈⠙⠲⣤⡀⠀⠀⠀⠀⠀⢸⡇⠀⠀⢸⡇⠀
+⠀⠀⠀⠀⡇⢰⣄⣤⣾⠀⠀⠀⢀⣠⠶⠋⠁⠀⢀⣾⣿⢿⣿⣾⣇⢹⡏⣻⣿⠞⠀⠀⠀⠰⣿⣏⣸⡇⣾⣿⣿⣿⣿⣿⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⢸⢳⣦⡞⢸⡇⠀
+⠀⠀⠀⠀⣷⡼⣯⡽⢿⣆⣤⣞⣋⣀⣀⣀⣀⣀⣸⣿⣿⣧⠬⠹⣯⣬⣿⠉⠹⣄⠀⠀⠀⣰⠏⠉⣿⢤⣿⠟⠲⣾⣿⣻⣧⣤⣤⣤⡤⠤⠤⠽⠿⢦⡼⠛⣷⠛⢿⠀⠀
+⠀⠀⠀⠀⢻⡄⠘⠃⠀⢿⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣷⣄⠀⢻⣿⠏⢦⠀⠈⠐⠀⠸⡁⠀⡟⠙⣿⠟⠀⣠⣾⣿⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠙⢀⡿⠀⠀
+⠀⠀⠀⠀⠘⣇⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣷⣄⠿⣄⠈⢿⡆⠀⠀⠀⢴⡿⠀⣠⠟⣠⣾⣿⢿⡽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⣸⠇⠀⠀
+⠀⠀⠀⠀⠀⢹⡆⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⢿⣶⡈⢦⢸⡇⠀⢠⠀⢸⡇⠐⢁⣼⣿⢿⣯⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠁⠀⠀⢠⡏⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢻⡄⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢶⣿⣿⣳⠀⠀⡇⠀⣼⠀⢸⡇⠀⣜⣿⣹⠶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠁⠀⠀⢀⡿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⡄⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⣯⡃⢸⡇⠀⢹⠂⠈⡇⠀⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠀⠀⠀⢠⡞⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠹⣆⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⠷⣼⠃⠀⠀⠀⠀⢷⡰⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⣠⠟⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⣄⠀⠀⠀⠀⠙⢦⣀⠀⠀⠀⠀⠀⠀⠀⢿⣴⣿⣦⣀⣠⣀⣤⣿⣧⢾⠆⠀⠀⠀⠀⠀⠀⠀⣠⠶⠃⠀⠀⠀⢀⡼⠋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⣆⣉⣷⢦⣀⠀⠀⠀⠀⢸⡜⠿⣷⣿⣿⠿⣽⡿⠛⡞⠀⠀⠀⠀⢀⣠⣴⢊⣁⠀⠀⠀⢀⣴⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠦⣄⣠⢿⣩⡷⡄⠈⠙⠓⠤⢤⣀⣙⣦⣈⣻⣦⣾⣁⣠⣞⣁⣀⠤⠴⠚⠋⣀⣿⣻⣧⡀⣀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠦⣟⡀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢿⡿⠷⣿⢿⡯⠉⠉⠀⠀⠀⠀⠀⠉⠉⣿⡾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠶⣄⣀⡀⠀⠀⠀⠀⠀⠙⣶⡿⢸⠇⠀⠀⠀⠀⣀⣠⠴⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠒⠒⠲⢾⣟⡥⠿⠒⠒⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀
+             _____                 ____              
+             / ___/__ ___ _  ___   / __ \_  _____ ____
+            / (_ / _ `/  ' \/ -_) / /_/ / |/ / -_) __/
+            \___/\_,_/_/_/_/\__/  \____/|___/\__/_/  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+"""
+
+# Call the delayed_print_green function with the specified text and delay
+
+
 
 class Spelare:
     def __init__(self):
@@ -19,30 +103,34 @@ class Spelare:
 
     def skada(self, antal_skada):
         self.hp -= antal_skada
-        print(f"Du har nu {self.hp} hp!")
+        
+    def död(self):
         if self.hp <= 0:
-            print(f"Game Over! Du har dött till {self.monster}")
+            return True
+        else:
+            return False
     
-    def ta_upp_vapen(self, Vapen):
-        self.vapen = Vapen
-        print(f"Du hitta ett {Vapen}")
+    def ta_upp_vapen(self, vapen):
+        self.vapen = vapen
+        print(f"Du hitta ett {vapen}")
     
-    def heal(self, Heals):
-        self.hp += Heals
-        print(f"Grattis! Du har nu hittat en heal som ger {Heals} hp och har nu {self.hp} HP")
+    def heal(self, heals):
+        self.hp += heals
+        if self.hp < 100:
+            self.hp = 100
 
 class Rum:
     def __init__(self):
         self.random = random.randint(1,13)
-        if self.random <= 4:
+        if self.random <= 6:
             self.chestvapen = None
             self.chestheals = None
             self.monster = random.choice(["Troll", "Robot", "Kloakråttor", "Zombie Jesper"]) 
 
-        elif self.random <= 8:
+        elif self.random <= 9:
             self.monster = None
             self.chestvapen = None
-            self.chestheals = random.choice(["Mini shield(25HP)", "Big pot(50HP)", "Chug Jug(100HP)"])
+            self.chestheals = random.choice(["Mini shield(15HP)", "Big pot(30HP)", "Chug Jug(60HP)"])
         else:
             self.monster = None
             self.chestheals = None
@@ -51,42 +139,75 @@ class Rum:
 
 def main():
     spelare = Spelare()
-    times_died = 0
+    
 
 
-
-    while times_died < 1:
+    while spelare.död() == False:
         
 
         print("Välj ett rum")
-        print("Rum 1")
-        print("Rum 2")
-        print("Rum 3")
+        print("1. Rum 1")
+        print("2. Rum 2")
+        print("3. Rum 3")
 
         Deras_val = input("-->")
-        if Deras_val == "1":
+        if Deras_val == "1" or Deras_val == "2" or Deras_val == "3":
             r = Rum()
             print(r.chestheals)
             print(r.chestvapen)
             print(r.monster)
             if r.monster != None:
-                val = input(f"Du har träffat på monstret {r.monster}. Vill du försöka springa eller slåss -->")
-                if val == "springa":
+                val = input(f"Du har träffat på monstret {r.monster}. Vill du försöka 1. springa bort eller 2. slåss -->")
+                if val == "springa bort" or val == "1":
                     antal_skada = random.randint(10,20)
                     print(f"Du hann springa bor men har tagit {antal_skada} skada")
-                    spelare.skada
-                    print(f"Du har nu {spelare.hp} hp!")
-                elif val == "slåss": 
+                    spelare.skada(antal_skada)
+                    if (spelare.död()):
+                            print(f"{text_to_print2}")
+                    else:
+                        print(f"Du har nu {spelare.hp} hp!")
+                elif val == "slåss" or val == "2": 
                     if spelare.vapen != None:
-                        antal_skada = random.randint(0,20)
+                        antal_skada = random.randint(0,30)
+                        spelare.skada(antal_skada)
+                        print(f"Du tog {antal_skada} skada")
+                        if (spelare.död()):
+                            delayed_print2_green(text_to_print2, delay)
+                        else:
+                            print(f"Du har nu {spelare.hp} hp!")
                     elif spelare.vapen == None:
-                        print("Du har dött och förlorat spelet")
-                        times_died = 1
+                        print(f"Du dog till {r.monster} eftersom att du inte hade något vapen och har därför förlorat spelet")
+                        antal_skada = 100
+                        spelare.skada(antal_skada)
+                        if (spelare.död()):
+                            delayed_print2_green(text_to_print2, delay)
+
                 
             
 
             elif r.chestvapen != None:
-                pass
+                if spelare.vapen == None:
+                    print(f"Grattis du har hittat vapnet {r.chestvapen}")
+                    spelare.vapen = r.chestvapen
+                else:
+                    vapen_val = input(f"Du har hittat vapnet {r.chestvapen}, vill du byta ut ditt vapen, {spelare.vapen}, för ett nytt vapen? ja eller nej-->")
+                    if vapen_val == "ja":
+                        spelare.vapen = r.chestvapen
+                        print(f"du har nu vapnet {spelare.vapen}")
+                    elif vapen_val == "nej":
+                        print(f"Du har kvar vapnet {spelare.vapen}")
+                    
+
+
+
+
+                
             elif r.chestheals != None:
-                pass
+                if r.chestheals == "Mini shield(15HP)":
+                    
+                    
+
+                    print(f"Grattis! Du har nu hittat en heal som ger hp och har nu {spelare.hp} HP")
 main()
+
+
